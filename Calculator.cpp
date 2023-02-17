c10d6e3552bbb51e651ab1f4319407d0593cf11d
alejandro_lopez24 <alejandro.lopez24@upr.edu>
2023-02-17 10:41:35 -0400

    created Caculator functions

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,2 +1,31 @@
-#include <iostream>
 #include <cmath>
+#include "Variables.h"
+
+double Var::getSum(){
+    double sum=(this->getVar1() + this->getVar2());
+    return sum;
+}
+
+double Var::getSub(){
+    double sub=(this->getVar1() - this->getVar2());
+    return sub;
+}
+
+double Var::getDiv(){
+    double div=(this->getVar1() / getVar2());
+    return div;
+}
+
+double Var::getMult(){
+    double mult=(this->getVar1() * this->getVar2());
+    return mult;
+}
+
+Var Var::getLargest(Var vars[]){
+    if(vars[0].getVar1() > vars[1].getVar2()){
+        return vars[0];
+    }
+    else {
+        return vars[1];
+    }
+}
